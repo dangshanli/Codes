cb06e64886b4095ff65b284dc5dc02b83af5e5a6
karas <1414377646@qq.com>
2021-12-13 17:09:42 +0800

    第一阶段重构

--- Refactor2/statement.js
+++ Refactor2/statement.js
@@ -1,61 +1,110 @@
-const fs = require('fs');
+const fs = require('fs')
 
-const invoices = JSON.parse(fs.readFileSync('./invoices.json'))[0];
-const plays = JSON.parse(fs.readFileSync('./plays.json'));
-const s = plays['hamlet'].type;
+const invoice = JSON.parse(fs.readFileSync('./invoices.json'))[0]
+const plays = JSON.parse(fs.readFileSync('./plays.json'))
+const s = plays['hamlet'].type
 // console.log(s);
-const result = statement(invoices, plays);
-console.log(result);
+const result = statement(invoice, plays)
+console.log(result)
 
 /**
- * 打印账单详情
- * @param {账单} invoice 
- * @param {剧目} play 
+ * 打印账单详情:
+ * - 计算价格
+ * - 计算积分
+ * @param {账单} invoice
+ * @param {剧目} play
  */
 function statement(invoice, plays) {
-    let totalAmount = 0;
-    let volumnCredits = 0;
+  let totalAmount = 0
+  let result = `statement for ${invoice.customer}\n` //账单客户
+  //遍历账单
+  for (let perf of invoice.performances) {
+    // volumnCredits += volumnCreditsFor(perf)
+    result += `${playFor(perf).name}:${usd(amountFor(perf))} (${
+      perf.audience
+    } seats)\n`
+    totalAmount += amountFor(perf)
+  }
 
-    let result = `statement for ${invoice.customer}\n`;//账单客户
-    const format = new Intl.NumberFormat( //打印格式
-        'zh-CN',
-        {
-            style: 'currency',
-            currency: 'USD',
-            minimunFractionDigits: 2
-        }).format;
+  //refactor:将积分计算拆出来
+  result += `Amount owned is ${usd(totalAmount)}\n`
+  result += `you earned ${totalVolumnCredits()} credits\n`//内联变量
+  return result
 
-    //遍历账单
+  /**
+   * 积分计算总数 分理出主函数
+   * @returns 
+   */
+  function totalVolumnCredits() {
+    let volumnCredits = 0
     for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
-        let thisAmount = 0;
-        //基本费用
-        switch (play.type) {
-            case "tragedy"://根据剧本类型计算总价格
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error(`unknow type:${play.type}`);
-        }
-        volumnCredits += Math.max(perf.audience - 30, 0);//基本积分
-        if ('comedy' == play.type) {//喜剧额外积分
-            volumnCredits += Math.floor(perf.audience / 5);
-        }
-
-        result += `${play.name}:${format(thisAmount / 100)} (${perf.audience} seats)\n`;
-        totalAmount += thisAmount;
+      volumnCredits += volumnCreditsFor(perf)
     }
-    result += `Amount owned is ${format(totalAmount / 100)}\n`
-    result += `you earned ${volumnCredits} credits\n`;
-    return result;
+    return volumnCredits
+  }
+
+  /**
+   * 计算积分
+   * @param {*} aPerformance
+   * @returns
+   */
+  function volumnCreditsFor(aPerformance) {
+    let result = 0
+    result += Math.max(aPerformance.audience - 30, 0) //基本积分
+    if ('comedy' === playFor(aPerformance).type) {
+      result += Math.floor(aPerformance.audience / 5)
+    }
+    return result
+  }
+
+  /**
+   * 内联函数
+   * @param {*} aPerformance
+   * @returns
+   */
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID]
+  }
+
+  /**
+   * refactor：extract function
+   * 计算单个剧本演出的费用
+   * @param {剧本} play
+   * @param {表演秀：包含多个剧本} perf
+   * @returns   表演秀中某个剧本的费用
+   */
+  function amountFor(aPerformance) {
+    //refactor:rename param and func_name
+    let result = 0
+    switch (playFor(aPerformance).type) {
+      case 'tragedy': //根据剧本类型计算总价格
+        result = 40000
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30)
+        }
+        break
+      case 'comedy':
+        result = 30000
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20)
+        }
+        result += 300 * aPerformance.audience
+        break
+      default:
+        throw new Error(`unknow type:${playFor(aPerformance).type}`)
+    }
+    return result
+  }
+}
+/**
+ * 格式化输出现金数值
+ * @param {number 美分} aNumber
+ * @returns
+ */
+function usd(aNumber) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimunFractionDigits: 2,
+  }).format(aNumber / 100) //转美元
 }
