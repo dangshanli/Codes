b58de98ecc11ce2d8ae9c81966b9c7ac056378e4
karas <1414377646@qq.com>
2022-06-21 00:17:30 +0800

    random greeting

--- go-crash/hello/hello.go
+++ go-crash/hello/hello.go
@@ -16,11 +16,10 @@ func main() {
 	log.SetPrefix("greetings:")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("")
+	message, err := greetings.Hello("Tom")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Println(message)
-
 }
