c809e313dd4deaafe074d5ac4dc2b9a3c266cff1
luzj <luzj@koal.com>
2022-06-22 17:24:59 +0800

    method interface package stub

---
+++ go-crash/hello/methodsInterfaces/methods.go
@@ -0,0 +1,18 @@
+package methodsinterfaces
+
+import (
+	"fmt"
+	"math"
+)
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func Me(){
+	fmt.Println("tell me")
+}
