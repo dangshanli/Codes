eaf74b6758d3c4dc8e7d02e8bd266fc1e541946a
karas <1414377646@qq.com>
2022-08-28 23:34:45 +0800

    生成二维数组工具类

---
+++ Claptrap/LetAlgorithm/src/main/java/dp/RobotCoinCollection.java
@@ -0,0 +1,9 @@
+package dp;
+
+// 机器人吃硬币
+public class RobotCoinCollection {
+
+    public static void main(String[] args) {
+
+    }
+}
--- Claptrap/LetAlgorithm/src/main/java/utils/P.java
+++ Claptrap/LetAlgorithm/src/main/java/utils/P.java
@@ -1,5 +1,8 @@
 package utils;
 
+import java.util.Random;
+import java.util.function.Supplier;
+
 public class P {
 
     // 模仿log.info的{}替代模式，格式化打印日志
@@ -29,4 +32,45 @@ public class P {
         }
         return result;
     }
+
+    public static <T> void showTwoDimension(T[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static <T> T[][] genTwoDimension(Supplier<T> supplier, T[][] result) {
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[i].length; j++) {
+                result[i][j] = supplier.get();
+            }
+        }
+        return result;
+    }
+
+    final static Random random = new Random();
+
+
+    public static void main(String[] args) {
+        Integer[][] a = genRobotCoinArr(10, 10);
+        showTwoDimension(a);
+    }
+
+    public static Integer[][] genRobotCoinArr(int x, int y) {
+        Integer[][] result = new Integer[x][y];
+        Supplier<Integer> supplier = () -> random.nextBoolean() ? 1 : 0;
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[i].length; j++) {
+                if (i == 0 || j == 0)
+                    result[i][j] = 0;
+                else
+                    result[i][j] = supplier.get();
+            }
+        }
+        return result;
+    }
+
 }
